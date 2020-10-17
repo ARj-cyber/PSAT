90230c99163d06ea976ba8258b244b4e590c3231
ARj-cyber <arjunpdinesh123@gmail.com>
2020-10-17 05:49:04 +0530

    Lab3 files

---
+++ lab3/lst_digit_of_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env/ python3
+
+
+
+
+#Problem 01 : Last digit of integer
+
+# Given an integer number, print its last digit. 
+
+
+num = input("Enter the number : ")
+last_digit = (int(num))%10
+print("the last digit is" , last_digit )
