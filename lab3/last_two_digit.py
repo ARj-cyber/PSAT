465ef79dd64075e979732dc28c1f5486718215e2
ARj-cyber <arjunpdinesh123@gmail.com>
2020-10-19 16:39:00 +0530

    LAST TWO DIGITS

--- lab3/last_two_digit.py
+++ lab3/last_two_digit.py
@@ -6,6 +6,6 @@
 
 print('Enter the number')
 numbe = input('> ')
-numbe = numbe+'0' # Concating with 0 for a correct output
+numbe = numbe+'0' # Concating with 0 for a correct proccesing of output
 lst_dgt = numbe[-3:-1]  # this ignores -3rd & -1st positioned characters
 print(lst_dgt)
