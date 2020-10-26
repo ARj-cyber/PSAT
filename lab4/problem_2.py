b6e92310576fd591cfdbfabe0c554976ab1b6685
ARj-cyber <arjunpdinesh123@gmail.com>
2020-10-26 10:00:37 +0530

    Problem 2

--- lab4/problem_2.py
+++ lab4/problem_2.py
@@ -7,3 +7,12 @@
  Read an integer,
  print "YES" if it is even and print "NO" otherwise.
 '''
+
+
+
+num = int(input("Enter the number : "))
+
+if num%2 == 0 :
+    print("YES")
+else:
+    print("NO")
