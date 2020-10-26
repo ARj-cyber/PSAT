b952fef3f0f05b69d6a87ce7bab8799576dfe7da
ARj-cyber <arjunpdinesh123@gmail.com>
2020-10-26 10:15:23 +0530

    Problem 6

---
+++ lab4/problem_6.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+# Problem 6
+# For the given integer X print 1 if it is positive, -1 if it is negative, or 0 if it is equal to zero. 
+
+
+num = int(input("Enter the number : ") )
+if (num >0) :
+    print("1")
+elif(num < 0) :
+    print("-1")
+else :
+    print("0")
